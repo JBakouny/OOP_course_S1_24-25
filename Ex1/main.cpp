45f48fe1f52bef953b0e70517d9e74f2f5595348
Youssef Bakouny <Youssef.Bakouny@usj.edu.lb>
2024-10-09 11:09:26 +0300

    Initial commit

--- Ex1/main.cpp
+++ Ex1/main.cpp
@@ -3,44 +3,7 @@
 using namespace std;
 
 
-void affiche(string texte, double x) {
-    cout << texte <<" " << x << endl;
-}
-
-void affiche(double x) {
-    affiche("Réel", x);
-}
-
-
-void affiche(int x, string texte) {
-    cout << "Entier: " << x << endl;
-}
-
-
 int main() {
-
-    int a = 2;
-
-    double b = 3.7;
-
-    affiche(a);
-    affiche("Integer", a);
-
-    affiche(b);
-
-
+	cout << "Hello" << endl;
+	return 0;
 }
-
-/*
-
-def affiche(x) :
-    print(x)
-
-
-def affiche(x, y):
-    print(x, "et", y)
-
-
-
-*/
-
