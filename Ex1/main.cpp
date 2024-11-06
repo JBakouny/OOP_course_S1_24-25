fade736d872d17a2763ae38d5f40868514afc914
Youssef Bakouny <Youssef.Bakouny@usj.edu.lb>
2024-11-06 09:46:12 +0200

    Implement dynamic memory allocation

--- Ex1/main.cpp
+++ Ex1/main.cpp
@@ -1,9 +1,21 @@
-#include <iostream>
-#include <string>
+#include<iostream>
 using namespace std;
 
-
 int main() {
-	cout << "Hello" << endl;
+	int* p = new int{ 37 };
+
+	cout << p << endl;
+	cout << *p << endl;
+	
+	delete p;
+	p = nullptr; // optionnel
+	p = new int{ 50 };
+
+
+	cout << p << endl;
+	cout << *p << endl;
+
+	delete p; // optionnel
+	p = nullptr;
 	return 0;
 }
