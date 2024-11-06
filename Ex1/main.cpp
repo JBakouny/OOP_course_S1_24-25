5083ecd6c12b74a636543f624def28a4c2447f7c
Youssef Bakouny <Youssef.Bakouny@usj.edu.lb>
2024-11-06 11:02:38 +0200

    Implement vector of pointers to cours

--- Ex1/main.cpp
+++ Ex1/main.cpp
@@ -1,21 +1,59 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
-int main() {
-	int* p = new int{ 37 };
-
-	cout << p << endl;
-	cout << *p << endl;
-	
-	delete p;
-	p = nullptr; // optionnel
-	p = new int{ 50 };
+struct Cours {
+    string nom;
+    int credits;
+};
 
 
-	cout << p << endl;
-	cout << *p << endl;
+void afficher(const Cours& c) {
+    cout << "Cours: " << c.nom << ", credits: " << c.credits << endl;
+}
 
-	delete p; // optionnel
-	p = nullptr;
-	return 0;
+struct Etudiant {
+    string nom;
+    int age;
+    vector<Cours*> matieres;
+};
+
+void afficher(const Etudiant& e) {
+    cout << e.nom << endl;
+    cout << "age: " << e.age << " ans" << endl;
+    cout << "Matieres:" << endl;
+    for (size_t i = 0; i < e.matieres.size(); ++i) {
+        afficher(*e.matieres[i]);
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Cours poo { "POO", 6 };
+
+    Cours progPar = { "ProgPar", 4 };
+
+    Cours progFun;
+    progFun.nom = "ProgFun";
+    progFun.credits = 4;
+
+    vector<Cours*> matieresJean = { &poo, &progPar };
+    Etudiant jean = { "Jean", 20, matieresJean };
+
+    Etudiant pierre;
+    pierre.nom = "Pierre";
+    pierre.age = 21;
+
+    pierre.matieres.push_back(&poo);
+    pierre.matieres.push_back(&progFun);
+
+    afficher(jean);
+    afficher(pierre);
+
+    poo.nom = "OOP";
+
+    afficher(jean);
+    afficher(pierre);
 }
